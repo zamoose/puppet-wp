42273ce4c57cbad0c5a8b6db6aea23c80b607c51
Doug Stewart <zamoose@gmail.com>
2014-02-18 23:46:35 -0500

    Create download.pp
    Initial commit.

---
+++ manifests/download.pp
@@ -0,0 +1,29 @@
+define wp::download (
+	$location = $title,
+	$locale         = false,
+	$version        = false,
+	$force          = false
+) {
+	include wp::cli
+
+	$download = ''
+
+	if ( $locale != false ) {
+		$s_locale = "--locale='$locale' "
+	}
+
+	if ( $version != false ) {
+		$s_version = "--version='$version' "
+	}
+
+	if ( $force != false ) {
+		$s_force = "--force"
+	}
+
+	exec {"wp core download $location":
+		command => "/usr/bin/wp core download $download $s_locale $s_version #s_force",
+		cwd => $location,
+		require => [ Class['wp::cli'] ],
+		creates => [ "$location/wp-admin", "$location/wp-content", "$location/wp-includes" ]
+	}
+}
